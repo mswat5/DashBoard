c0a7859c707f605d440e850294afddc56f3f1b3d
mswat5 <mswat14361@gmail.com>
2023-09-20 01:10:05 +0530

    Dashboard

---
+++ src/components/SideMenuItems.tsx
@@ -0,0 +1,15 @@
+
+const SideMenuItems = () => {
+  return (
+    <>
+<div>lorem</div>
+<div>lorem</div>
+<div>lorem</div>
+<div>lorem</div>
+<div>lorem</div>
+<div>lorem</div>
+    </>
+  )
+}
+
+export default SideMenuItems
